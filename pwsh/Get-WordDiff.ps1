e5723af3478106b04c0a45622dc04c418c19e40c
ludwhe <_@ludwhe.dev>
2023-01-05 16:22:45 +0100

    Parametrize main branch name

--- pwsh/Get-WordDiff.ps1
+++ pwsh/Get-WordDiff.ps1
@@ -1,1 +1,8 @@
-git diff --word-diff master | Select-String '\[-.+?-]\{\+.+?\+\}' | ForEach-Object { Write-Output $_.Matches.Value }
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $MainBranchName = "main"
+)
+
+git diff --word-diff $MainBranchName | Select-String '\[-.+?-]\{\+.+?\+\}' | ForEach-Object { Write-Output $_.Matches.Value }
